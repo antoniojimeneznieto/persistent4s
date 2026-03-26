f909c0690b1601b0aef4cfa047282ca42cbd8e17
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2026-03-26 11:12:56 +0100

    Define implicits EventSore to avoid `using`parameter in every service.

---
+++ modules/testkit/src/main/scala/persistent4s/testkit/implicits.scala
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2026 persistent4s
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package persistent4s.testkit
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+
+object implicits:
+
+  implicit def store[A]: InMemoryEventStore[IO, A] = InMemoryEventStore.make[IO, A].unsafeRunSync()
